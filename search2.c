4b91c88eaebf1fbfa25973df9b5ee6e571fc8b66
dlrud <65211274+dlrud@users.noreply.github.com>
2020-06-14 23:48:46 +0900

    Update search2.c

--- search2.c
+++ search2.c
@@ -6,7 +6,7 @@ int search(phone* fri)
     printf("검색할 이름 : ");
     scanf("%s", who);
 
-    for (int i = 0; i < NAME; i++)
+    for (int i = 0; i < 100; i++) // 저장 가능한 사람을 100명으로 가정
         {
             if (!strcmp(who, fri[i].name))
             {
