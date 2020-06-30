8e4f8a8c2564a11bd8ac0d53fb509d2c6677843f
nexmin0805 <65328995+nexmin0805@users.noreply.github.com>
2020-06-30 20:12:57 +0900

    Create input.php

---
+++ input.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>문제5. 데이터 입력받아 화면에 출력하기</h1>
+    <form method="POST" action="save.php">
+        이름: <input type="text" name="name"/><br/>
+        전화번호: <input type="text" name="tel"/><br/>
+
+        <input type="submit" name="submit"/>
+    </form>
+
+</body>
+</html>
+
+
